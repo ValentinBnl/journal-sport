a31eee13dc8d447b485e7f9afe2519c70704c15f
Valentin Bunel <bunel.valentin@hotmail.fr>
2025-01-15 14:47:28 +0100

    first commit

---
+++ src/Entity/Activity.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ActivityRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ActivityRepository::class)]
+class Activity
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $typeSport = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $activityType = null;
+
+    #[ORM\Column]
+    private ?float $duration = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $notes = null;
+
+    #[ORM\Column]
+    private ?int $difficulty = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTypeSport(): ?string
+    {
+        return $this->typeSport;
+    }
+
+    public function setTypeSport(string $typeSport): static
+    {
+        $this->typeSport = $typeSport;
+
+        return $this;
+    }
+
+    public function getActivityType(): ?string
+    {
+        return $this->activityType;
+    }
+
+    public function setActivityType(?string $activityType): static
+    {
+        $this->activityType = $activityType;
+
+        return $this;
+    }
+
+    public function getDuration(): ?float
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(float $duration): static
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getNotes(): ?string
+    {
+        return $this->notes;
+    }
+
+    public function setNotes(?string $notes): static
+    {
+        $this->notes = $notes;
+
+        return $this;
+    }
+
+    public function getDifficulty(): ?int
+    {
+        return $this->difficulty;
+    }
+
+    public function setDifficulty(int $difficulty): static
+    {
+        $this->difficulty = $difficulty;
+
+        return $this;
+    }
+}
